fbb053fb2520f923efdb9920d046bbee5c216c36
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-08-27 22:59:10 +0200

    RMX2155: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: oplus_mssi_64_cn Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for realme oplus_mssi_64_cn (RMX2155)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_RMX2155.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from RMX2155 device
+$(call inherit-product, device/realme/RMX2155/device.mk)
+
+PRODUCT_DEVICE := RMX2155
+PRODUCT_NAME := omni_RMX2155
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := oplus_mssi_64_cn
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1614320228960 release-keys"
+
+BUILD_FINGERPRINT := alps/vnd_oppo6785/oppo6785:11/RP1A.200720.011/1614320228960:user/release-keys
