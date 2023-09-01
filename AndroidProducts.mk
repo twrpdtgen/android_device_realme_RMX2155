337c1b05d49656f20c5e8c3f650f04fc750495eb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-01 08:44:49 +0200

    RMX2155: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: RMX2155 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RMX2155.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RMX2155-user \
+    omni_RMX2155-userdebug \
+    omni_RMX2155-eng
